63b48d04cd4efe565ba62471a47b7e85d2d509ee
lqh <lqh@spzh.com>
2021-01-07 16:36:01 +0800

    config the roboletric unit test environment failed! for: 1 modify the robolectric.properties 's dependency.url property not work!   for which offical docs say it can be done!!!   2. can get the flow of codes

---
+++ app/src/test/java/InstrumentTestTest.kt
@@ -0,0 +1,16 @@
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+//@RunWith(AndroidJUnit4::class)
+class InstrumentTestTest {
+
+    @Test
+    fun testNothing(){
+        println("test nothing")
+    }
+}
