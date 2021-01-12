e573dce9366888381b4cf91d89928c9ecc0ac5ea
wilson.goodfellow <heimoheimo@126.com>
2021-01-12 23:53:26 +0800

    update FileView widget

--- app/src/main/java/com/example/dashboard_kv/widget/FileView.kt
+++ app/src/main/java/com/example/dashboard_kv/widget/FileView.kt
@@ -7,9 +7,12 @@ import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.example.dashboard_kv.R
 
 
 /**
@@ -23,96 +26,48 @@ class FileView(context:Context) : LinearLayout(context) {
     private lateinit var image:ImageView
     private lateinit var tvFileName:TextView
 
-    constructor(context: Context,attributeSet: AttributeSet):this(context){
+    @SuppressLint("ResourceAsColor")
+    constructor(context: Context, attributeSet: AttributeSet):this(context){
 
-        //this.orientation = VERTICAL
-        //this.layoutParams.width = WRAP_CONTENT
-        //this.layoutParams.height = WRAP_CONTENT
+        orientation = VERTICAL
+
+        this.layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
 
         val ta = context.obtainStyledAttributes(attributeSet,
                 com.example.dashboard_kv.R.styleable.FileView, 0, 0)
 
-        //can set default value for view widget
-        //imgSrouce = ta.getString(com.example.dashboard_kv.R.styleable.FileView_imgSource)
-
-
-
         image = ImageView(context)
             .apply {
 
-                //this.layoutParams.width = WRAP_CONTENT
-                //this.layoutParams.height = WRAP_CONTENT
+                this.layoutParams = LayoutParams(50,50)
 
-                minimumWidth =50
-                maxWidth = 70
-                minimumHeight = 50
-                maxHeight = 70
                 scaleType = ImageView.ScaleType.FIT_CENTER
-
-                val img  = resources.getDrawable(com.example.dashboard_kv.R.drawable.file) as BitmapDrawable
+                val img  = resources.getDrawable(R.drawable.file) as BitmapDrawable
                 setImageDrawable(img)
 
             }.also {
-                addView(it)
+                this.addView(it)
             }
 
-//        tvFileName = TextView(context)
-//            .apply {
-//                 text = ta.getString(com.example.dashboard_kv.R.styleable.FileView_fileName)
-//                width = WRAP_CONTENT
-//                height = WRAP_CONTENT
-//            }.also {
-//                this.addView(it)
-//            }
+        tvFileName = TextView(context)
+            .apply {
+                layoutParams = LayoutParams(50,20)
+                background  = resources.getDrawable(R.drawable.default_background)
+                //text = ta.getString(com.example.dashboard_kv.R.styleable.FileView_fileName)
+                text = "Hello World"
+                textSize = 30.0f
+                setTextColor(R.color.lb_default_search_color)
+
+            }.also {
+                this.addView(it)
+            }
 
     }
 
 
-    private fun getMeasurementSize(measureSpec: Int, defaultSize: Int): Int {
-        val mode = MeasureSpec.getMode(measureSpec)
-        val size = MeasureSpec.getSize(measureSpec)
-        return when (mode) {
-            MeasureSpec.EXACTLY -> size
-            MeasureSpec.AT_MOST -> Math.min(defaultSize, size)
-            MeasureSpec.UNSPECIFIED -> defaultSize
-            else -> defaultSize
-        }
-    }
-
-     val  DEFAULT_SIZE =560
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-       // setMeasuredDimension(100,100)
 
 
-        //super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        /*
-        val width = getMeasurementSize(widthMeasureSpec, DEFAULT_SIZE)
-        val height = getMeasurementSize(
-            heightMeasureSpec,
-            DEFAULT_SIZE
-        )
-        setMeasuredDimension(width, height)
 
-         */
-    }
-
-    override fun addViewInLayout(
-        child: View?,
-        index: Int,
-        params: ViewGroup.LayoutParams?
-    ): Boolean {
-        return super.addViewInLayout(child, index, params)
-    }
-
-    @SuppressLint("UseCompatLoadingForDrawables")
-    override fun onDraw(canvas: Canvas?) {
-
-        super.onDraw(canvas)
-
-    }
 
 
 }
